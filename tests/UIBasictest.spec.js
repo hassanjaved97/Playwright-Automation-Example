7580eac58a189f6793e5ec3e14eabaf952566614
Hafiz Hassan Javed <hassanmian120@gmail.com>
2026-08-25 17:46:56 +0500

    Fixed the login test

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -26,7 +26,7 @@ await expect(page).toHaveTitle("Google");
 
 });
 
-test.only ('test case 1', async ({page})=>
+test ('test case 1', async ({page})=>
 
     {
         const userName = page.locator('input#username');
