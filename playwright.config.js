9d30b5773c9542189179913a7fdf389a06c0f5ea
Hafiz Hassan Javed <hassanmian120@gmail.com>
2026-08-25 17:23:29 +0500

    Initial commit: Playwright test suite scaffold

---
+++ playwright.config.js
@@ -0,0 +1,33 @@
+// @ts-check
+const { defineConfig, devices, chromium } = require('@playwright/test');
+
+
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+const config = {
+  testDir: './tests',
+  timeout: 40*1000,
+
+  expect: {
+
+      timeout: 5000,
+
+  },
+
+  reporter:'html',
+  
+  use: {
+    browserName: 'chromium',
+    headless : false
+
+   
+  },
+
+  
+};
+
+
+module.exports = config;
+
---
+++ tests/UIBasictest.spec.js
@@ -0,0 +1,70 @@
+const {test, expect} = require ('@playwright/test');
+
+test('Browser Context Playwright test', async ({browser})=>
+
+{
+
+const context = await browser.newContext();
+const page = await context.newPage();
+await page.goto("https://fnint.com");
+console.log(await page.title());
+await expect(page).toHaveTitle("FutureNow Technologies");
+
+
+});
+
+test('Page Playwright test', async ({page})=>
+
+{
+
+
+await page.goto("https://google.com");
+console.log(await page.title());
+await expect(page).toHaveTitle("Google");
+
+
+
+});
+
+test.only ('test case 1', async ({page})=>
+
+    {
+        const userName = page.locator('input#username');
+        const signIn = page.locator('input#signInBtn');
+        const pass = page.locator('input#password');
+        const cardTitles = page.locator(".card-body a");
+        await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+        console.log(await page.title());
+        // Css xpath
+        await userName.type("Umair");
+        await pass.type("Learning@830$3mK2");
+        await signIn.click();
+        
+        console.log(await page.locator("[style*='block']").textContent());
+        await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+       // type - fill
+
+        await userName.fill("");
+        await userName.fill("rahulshettyacademy");
+
+        await Promise.all(
+
+    [    
+        page.waitForNavigation(),
+        await signIn.click(),
+
+    ]
+        );
+
+        
+        // console.log (await cardTitles.nth(0).textContent());
+        // await expect(await cardTitles.nth(0)).toContainText('iphone X');
+        await page.waitForLoadState('Networkidle');
+        const allTitles = await cardTitles.allTextContents();
+        console.log(allTitles); 
+        
+
+
+    });
+
+    
