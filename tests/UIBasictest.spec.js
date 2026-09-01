c9d8d6d97f9928df1bea8bcb0dbf921d6765ba1b
Hafiz Hassan Javed <hassanmian120@gmail.com>
2026-09-01 14:43:00 +0500

    Fixed the login test

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -36,8 +36,8 @@ test ('test case 1', async ({page})=>
         await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
         console.log(await page.title());
         // Css xpath
-        await userName.type("Umair");
-        await pass.type("Learning@830$3mK2");
+        await userName.fill("Umair");
+        await pass.fill("Learning@830$3mK2");
         await signIn.click();
         
         console.log(await page.locator("[style*='block']").textContent());
@@ -50,8 +50,8 @@ test ('test case 1', async ({page})=>
         await Promise.all(
 
     [    
-        page.waitForNavigation(),
-        await signIn.click(),
+        //page.waitForNavigation(),
+        //await signIn.click(),
 
     ]
         );
@@ -67,4 +67,35 @@ test ('test case 1', async ({page})=>
 
     });
 
+test.only ('UI Controls', async ({page})=>
+
+{
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const userName = page.locator('input#username');
+    const signIn = page.locator('input#signInBtn');
+    const pass = page.locator('input#password');
+
+    const dropdown = page.locator("select.form-control");
+
+    await dropdown.selectOption("Teacher");
+
+    await page.locator(".radiotextsty").last().click();
+
+    await expect(page.locator(".radiotextsty").last()).toBeChecked();
     
+    // console.log (page.locator(".radiotextsty").last().isChecked());
+    await page.locator(("#okayBtn")).click();
+
+
+    await page.locator("#terms").click();
+
+    await expect(page.locator("#terms")).toBeChecked();
+
+    await page.locator("#terms").uncheck();
+
+    expect (await page.locator("#terms").isChecked()).toBeFalsy();
+    
+    await page.pause();
+
+    
+ });
