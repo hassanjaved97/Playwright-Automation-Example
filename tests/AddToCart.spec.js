38a289c5edb22b3a393c9a4788da8ed111c669a6
Hafiz Hassan Javed <hassanmian120@gmail.com>
2026-09-09 13:25:33 +0500

    Fixed the login test

--- tests/AddToCart.spec.js
+++ tests/AddToCart.spec.js
@@ -4,16 +4,18 @@ test ('Client App Login', async ({page}) =>
 
     {
         
-    const productName = 'ZARA COAT 3';   
+    const productName = 'iphone 13 pro';   
     const products = page.locator(".card-body");    
     await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
     await page.locator("#userEmail").fill("hassan102@gmail.com");
     await page.locator("#userPassword").fill("Pakistan@123");
     await page.locator("#login").click();
     await page.waitForLoadState('networkidle');
+
     const titles = await page.locator(".card-body b").allTextContents();
     console.log(titles);
     const count = await products.count();
+
 
     for (let i=0; i< count; ++i)
 
@@ -33,8 +35,11 @@ test ('Client App Login', async ({page}) =>
 
 await page.locator("[routerlink$='/dashboard/cart']").click();
 await page.locator("div li").first().waitFor();
-const bool =  page.locator("h3:has-text('productName')").isVisible();
-expect(bool).toBeTruthy();
-  //await page.pause();
+const bool = await page.locator(`h3:has-text('${productName}')`).isVisible();expect(bool).toBeTruthy();
+await page.locator("text=Checkout").click();
+await page.locator("[value$='4542 9931 9292 2293']").fill("");
+await page.locator("[value$='4542 9931 9292 2293']").fill("1234 5678 0000 2222");
+
+//await page.pause();
 
   });
