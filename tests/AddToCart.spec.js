aadc71b408245a89aaa692ef785bb362326a5da9
Hafiz Hassan Javed <hassanmian120@gmail.com>
2026-09-10 16:22:47 +0500

    Fixed the login test

--- tests/AddToCart.spec.js
+++ tests/AddToCart.spec.js
@@ -1,4 +1,5 @@
 const {test, expect} = require ('@playwright/test');
+const { text } = require('node:stream/consumers');
 
 test ('Client App Login', async ({page}) =>
 
@@ -34,11 +35,51 @@ test ('Client App Login', async ({page}) =>
         }
 
 await page.locator("[routerlink$='/dashboard/cart']").click();
-await page.locator("div li").first().waitFor();
-const bool = await page.locator(`h3:has-text('${productName}')`).isVisible();expect(bool).toBeTruthy();
+
+await page.locator("div li").nth(0).waitFor();
+
+//const bool = await page.locator(`h3:has-text('${productName}')`).isVisible();
+// expect(bool).toBeTruthy();
+
+await expect(page.locator(`h3:has-text('${productName}')`)).toBeVisible();
+
 await page.locator("text=Checkout").click();
 await page.locator("[value$='4542 9931 9292 2293']").fill("");
 await page.locator("[value$='4542 9931 9292 2293']").fill("1234 5678 0000 2222");
+const dropdown1 = page.locator("select.input.ddl").first();
+await dropdown1.selectOption("07");
+const dropdown2 = page.locator("select.input.ddl").last();
+await dropdown2.selectOption("31");
+const cvv = page.locator('div.field.small', { hasText: 'CVV Code' }).locator('input');
+await cvv.fill("123");
+
+const CardName = page.locator('div.field', { hasText: 'Name on Card ' }).locator('input');
+await CardName.fill("Hassan javed");
+
+await page.locator("[placeholder$='Select Country']").type("pak", {delay:100});
+
+
+const dropdown = page.locator(".ta-results");
+await dropdown.waitFor();
+const optionsCount = await dropdown.locator("button").count();
+
+for (let i=0; i<optionsCount; ++i)
+
+    {
+      const text = await dropdown.locator("button").nth(i).textContent();
+
+        if (text.trim() === "Pakistan")
+
+            {
+
+                //click
+                await dropdown.locator("button").nth(i).click();
+                break;
+            }
+
+    }
+
+
 
 //await page.pause();
 
