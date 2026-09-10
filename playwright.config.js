7f7391203927502f0688a31a5184191f53823dcc
Hafiz Hassan Javed <56250682+hassanjaved97@users.noreply.github.com>
2026-09-10 16:26:49 +0500

    Fix: Enable headless mode for CI environment

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,7 @@ const config = {
   
   use: {
     browserName: 'chromium',
-    headless : false,
+    headless : true,
     screenshot : 'on',
     trace: 'retain-on-failure' //on - off
     // trace: 'on'
